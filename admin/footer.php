9771f1b05943fec357eb6e5b7ad2ce156dc5c6d1
SakshiKagda <kagdasakshi09@gmail.com>
2024-01-21 23:34:40 +0530

    files added

---
+++ admin/footer.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>  
+</head>
+<body>
+<footer class="footer">
+  <div class="container-fluid d-flex justify-content-between">
+    <span class="text-muted d-block text-center text-sm-start d-sm-inline-block">Copyright © expensetracker.com 2024</span>
+    <span class="text-muted d-block text-center text-sm-start d-sm-inline-block">Designed and Developed by Priya & Sakshi</span>
+  </div>
+</footer>
+ <!-- plugins:js -->
+ <script src="assets/vendors/js/vendor.bundle.base.js"></script>
+    <!-- endinject -->
+    <!-- Plugin js for this page -->
+    <script src="assets/vendors/chart.js/Chart.min.js"></script>
+    <script src="assets/js/jquery.cookie.js" type="text/javascript"></script>
+    <!-- End plugin js for this page -->
+    <!-- inject:js -->
+    <script src="assets/js/off-canvas.js"></script>
+    <script src="assets/js/hoverable-collapse.js"></script>
+    <script src="assets/js/misc.js"></script>
+    <!-- endinject -->
+    <!-- Custom js for this page -->
+    <script src="assets/js/dashboard.js"></script>
+    <script src="assets/js/todolist.js"></script>
+    <!-- End custom js for this page -->
+</body>
+</html>
