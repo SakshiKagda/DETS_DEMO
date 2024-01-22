c9eee4883ab04d7e776275633cb2c20e69f31f82
SakshiKagda <kagdasakshi09@gmail.com>
2024-01-22 15:44:50 +0530

    sidebar.php

--- admin/sidebar.php
+++ admin/sidebar.php
@@ -46,6 +46,20 @@
         </ul>
       </div>
     </li>
+    <li class="nav-item">
+      <a class="nav-link" data-bs-toggle="collapse" href="#ui-basic" aria-expanded="false" aria-controls="ui-basic">
+        <span class="menu-title">Budget</span>
+        <i class="menu-arrow"></i>
+        <i class=" mdi mdi-cash  menu-icon" ></i>
+      </a>
+      <div class="collapse" id="ui-basic">
+        <ul class="nav flex-column sub-menu">
+          <li class="nav-item"> <a class="nav-link" href="pages/ui-features/addbudget.php">Add Budget</a></li>
+          <li class="nav-item"> <a class="nav-link" href="pages/ui-features/viewbudget.php">Manage Budget</a></li>
+        </ul>
+      </div>
+    </li>
+    
     
 
 
