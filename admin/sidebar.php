c1e86aeed6bf10be8af3fcb4f1e05a4deb74696e
SakshiKagda <kagdasakshi09@gmail.com>
2024-01-22 15:42:07 +0530

    sidebar.php

--- admin/sidebar.php
+++ admin/sidebar.php
@@ -46,19 +46,7 @@
         </ul>
       </div>
     </li>
-    <li class="nav-item">
-      <a class="nav-link" data-bs-toggle="collapse" href="#ui-basic" aria-expanded="false" aria-controls="ui-basic">
-        <span class="menu-title">Income</span>
-        <i class="menu-arrow"></i>
-        <i class=" mdi mdi-cash  menu-icon" ></i>
-      </a>
-      <div class="collapse" id="ui-basic">
-        <ul class="nav flex-column sub-menu">
-          <li class="nav-item"> <a class="nav-link" href="pages/ui-features/addincome.php">Add Income</a></li>
-          <li class="nav-item"> <a class="nav-link" href="pages/ui-features/viewincome.php">Manage Income</a></li>
-        </ul>
-      </div>
-    </li>
+    
 
 
   </ul>
