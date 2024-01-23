f83561213ad242dacf1d1ad89d5d074db89ad066
SakshiKagda <kagdasakshi09@gmail.com>
2024-01-23 18:29:05 +0530

    commited

---
+++ admin/viewreport.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <header>
+        <?php
+        include("header.php");
+        ?>
+    </header>
+    <sidebar>
+    <?php
+        include("sidebar.php");
+        ?>
+    </sidebar>
+    <footer>
+    <?php
+        include("footer.php");
+        ?>
+    </footer>
+</body>
+</html>
