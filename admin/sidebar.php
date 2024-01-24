1f6a673f36b092206e5b608e90245bbfa43d8729
SakshiKagda <kagdasakshi09@gmail.com>
2024-01-24 12:29:43 +0530

    commited

--- admin/sidebar.php
+++ admin/sidebar.php
@@ -6,7 +6,7 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Document</title>
   <link rel="stylesheet" href="assets/scss/_sidebar.scss">
- 
+  <link rel="stylesheet" href="assets/css/style.css">
   
   <link rel="stylesheet" href="assets/vendors/mdi/css/materialdesignicons.min.css">
   <link rel="stylesheet" href="assets/vendors/css/vendor.bundle.base.css">
